02d386aeb855f3558abbeff6406f7c28eea80fb5
joshmun <joshuamun@gmail.com>
2017-04-17 00:46:07 -0700

    Edit migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417051848) do
+ActiveRecord::Schema.define(version: 20170417074522) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg"
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
